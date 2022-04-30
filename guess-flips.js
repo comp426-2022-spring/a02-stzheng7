3c388e1a0ec9341ec0c2b6eb52ef8e4df3af0559
stzheng7 <69952035+stzheng7@users.noreply.github.com>
2022-04-30 10:29:28 -0400

    fixes

--- guess-flips.js
+++ guess-flips.js
@@ -1,13 +1,11 @@
 import { flipACoin } from "./modules/coin.mjs";
-import { createRequire } from "module";
+import minimist from "minimist";
 
-const require = createRequire(import.meta.url);
-const args = require('minimist')(process.argv.slice(2));
-args['call'];
-const call = args.call;
-if (call != "heads" && call != "tails") {
-    console.log("Error: no input. \n Usage: node guess-flip --call=[heads|tails]")
+const args = minimist(process.argv.slice(2));
+const call = args["call"]; || null;
+if (call != "heads" || call != "tails") {
+    console.log(flipACoin(call));
 }
 else {
-    console.log(flipACoin(call));
+    console.log("Error: no input. \n Usage: node guess-flip --call=[heads|tails]")
 }
