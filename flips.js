e0e81040df120b336df4c08780e7f191af890ab7
stzheng7 <69952035+stzheng7@users.noreply.github.com>
2022-04-30 10:50:15 -0400

    fixes

--- flips.js
+++ flips.js
@@ -2,8 +2,9 @@ import { coinFlips, countFlips } from "./modules/coin.mjs";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
-args['number'];
+args["number"];
 const number = args.number || 1;
+
 const FlipsA = coinFlips(number);
 
 console.log(FlipsA);
